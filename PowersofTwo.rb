b6748e13c61325595f2e64f83a5845125c7be092
jackiehoon <wognsdl4545@naver.com>
2015-07-31 18:59:00 +0900

    Create PowersofTwo.rb

---
+++ PowersofTwo.rb
@@ -0,0 +1,19 @@
+def PowersofTwo(num)
+  while 1
+    if num%2 !=0
+      hot=false
+      return hot
+    elsif num == 2
+      hot = true
+      return hot
+    else
+      num=num/2
+    end
+  end
+    
+
+
+  # code goes here
+  return hot
+         
+end
